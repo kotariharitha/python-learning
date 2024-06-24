887a25e983bede97fee0552cbe375472d64ee761
haritha kotari <haritha.rao305@gmail.com>
2024-06-24 14:47:06 -0400

    square numbers - hackerrank

---
+++ exam.py
@@ -0,0 +1,13 @@
+# Read an integer input from the user
+n = int(input("Enter an integer: "))
+
+# Conditional checks based on the given rules
+if n % 2 != 0:
+    print("Weird")
+else:
+    if 2 <= n <= 5:
+        print("Not Weird")
+    elif 6 <= n <= 20:
+        print("Weird")
+    elif n > 20:
+        print("Not Weird")
