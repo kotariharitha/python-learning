3f64990b823ac89a34153ffd7ef47e2429c99ec4
haritha kotari <haritha.rao305@gmail.com>
2024-06-24 14:35:30 -0400

    initial python code for average

---
+++ average.py
@@ -0,0 +1,4 @@
+'''import statistics
+print(statistics.mean([100, 90]))'''
+import sys
+print("hi", "my name is", sys.argv[2]) # here you have to execute the code like this python libraries.py hari 
