8c95a951446e9cda94f03b4f221937a92839265c
haritha kotari <haritha.rao305@gmail.com>
2024-06-24 14:50:43 -0400

    corrected input

--- exam.py
+++ exam.py
@@ -1,5 +1,5 @@
 # Read an integer input from the user
-n = int(input("Enter an integer: "))
+n = int(input()) # "Enter an integer: "
 
 # Conditional checks based on the given rules
 if n % 2 != 0:
@@ -11,3 +11,13 @@ else:
         print("Weird")
     elif n > 20:
         print("Not Weird")
+
+# Original list
+numbers = [1, 2, 3, 4, 5]
+
+# Squaring each element using a for loop
+squared_numbers = []
+for x in numbers:
+    squared_numbers.append(x**2)
+
+print(squared_numbers)  # Output: [1, 4, 9, 16, 25]
