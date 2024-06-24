dcd7dd1b9e69d6c8a375c286cb10cd5ebc1d4c1c
haritha kotari <haritha.rao305@gmail.com>
2024-06-24 14:56:17 -0400

    random library

---
+++ libraries.py
@@ -0,0 +1,13 @@
+#import random
+#coin = random.choice(["heads", "tails"])
+#print(coin)
+'''import random
+number = random.randint(1, 10)
+print(number)'''
+'''import random
+cards = ["queen", "king", "jocker", "manger"]
+random.shuffle(cards)
+for card in cards:
+    print(card)'''
+import sys
+print("hi", "my name is", sys.argv[1])
