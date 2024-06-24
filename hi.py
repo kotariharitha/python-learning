69c2ec944d5a54a3365344e8d46e15951d0eab28
haritha kotari <haritha.rao305@gmail.com>
2024-06-24 14:55:05 -0400

    updated

--- hi.py
+++ hi.py
@@ -11,3 +11,17 @@ if y == 0:
         print("Not Weird")
 else:
     print("Weird")
+#====
+# Read an integer input from the user
+n = int(input("Enter an integer: "))
+
+# Conditional checks based on the given rules
+if n % 2 != 0:
+    print("Weird")
+else:
+    if 2 <= n <= 5:
+        print("Not Weird")
+    elif 6 <= n <= 20:
+        print("Weird")
+    elif n > 20:
+        print("Not Weird")
